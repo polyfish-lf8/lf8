4213b0b49ae5f695a57ec7e2105a47d63ef8b321
Sören Schwerk <s.schwerk@schule.bremen.de>
2021-10-11 16:26:04 +0200

    funktionsfähiges Starterprojekt

---
+++ src/main/java/de/szut/lf8_project/Lf8ProjectApplication.java
@@ -0,0 +1,13 @@
+package de.szut.lf8_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lf8ProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lf8ProjectApplication.class, args);
+    }
+
+}
