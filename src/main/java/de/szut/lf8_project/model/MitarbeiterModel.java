37227ee1eb7f4bc2e9d4559654e5c7dabd853875
„Jana“ <jana@werk85.de>
2023-10-04 10:00:26 +0200

    wip

---
+++ src/main/java/de/szut/lf8_project/model/MitarbeiterModel.java
@@ -0,0 +1,4 @@
+package de.szut.lf8_project.model;
+
+public class MitarbeiterModel {
+}
---
+++ src/main/java/de/szut/lf8_project/model/ProjektModel.java
@@ -0,0 +1,11 @@
+package de.szut.lf8_project.model;
+
+import java.util.List;
+
+public class ProjektModel {
+    private int id, kunden_id, va_mitarbeiter_id, va_kunden_id;
+    private String bezeichnung;
+    private List<MitarbeiterModel> Mitarbeiter;
+    private
+
+}
