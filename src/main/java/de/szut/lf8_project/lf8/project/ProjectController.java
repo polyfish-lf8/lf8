c73ab89fafeac646365f72626bd8fa8fa6681f68
Vrglab <Vrglab1@gmail.com>
2023-10-04 12:37:02 +0200

    Added Annotations to the create function in ProjectController.java

--- src/main/java/de/szut/lf8_project/lf8/project/ProjectController.java
+++ src/main/java/de/szut/lf8_project/lf8/project/ProjectController.java
@@ -1,6 +1,13 @@
 package de.szut.lf8_project.lf8.project;
 
+import de.szut.lf8_project.hello.dto.HelloGetDto;
 import de.szut.lf8_project.lf8.project.dto.ProjectCreateDto;
+import de.szut.lf8_project.lf8.project.dto.ProjectGetDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +26,18 @@ public class ProjectController {
         this.mapper = mapper;
     }
 
-    public ProjectCreateDto create(@RequestBody @Valid ProjectCreateDto dto) {
+    @Operation(summary = "Creates a new ProjectEntity using its required parameters")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode =  "201", description = "creation successful", content = {
+                    @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ProjectGetDto.class))
+            }),
+            @ApiResponse(responseCode = "400", description = "invalid JSON posted",
+                    content = @Content),
+            @ApiResponse(responseCode = "401", description = "not authorized",
+                    content = @Content)
+    })
+    public ProjectGetDto create(@RequestBody @Valid ProjectCreateDto dto) {
         return null;
     }
 }
