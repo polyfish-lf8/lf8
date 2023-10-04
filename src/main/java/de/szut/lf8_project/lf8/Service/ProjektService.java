9eb8ae284e66d2167a401882760128fa3230b1c2
Vrglab <Vrglab1@gmail.com>
2023-10-04 10:45:15 +0200

    Basic Service

--- src/main/java/de/szut/lf8_project/lf8/Service/ProjektService.java
+++ src/main/java/de/szut/lf8_project/lf8/Service/ProjektService.java
@@ -1,4 +1,38 @@
 package de.szut.lf8_project.lf8.Service;
 
+import de.szut.lf8_project.hello.HelloEntity;
+import de.szut.lf8_project.lf8.Entity.ProjektEntity;
+import de.szut.lf8_project.lf8.Repository.ProjektRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ProjektService {
+    private final ProjektRepository repository;
+
+    public ProjektService(ProjektRepository repository) {
+        this.repository = repository;
+    }
+
+    public ProjektEntity create(ProjektEntity entity) {
+        return  repository.save(entity);
+    }
+
+    public List<ProjektEntity> readAll() {
+        return this.repository.findAll();
+    }
+
+    public ProjektEntity readById(long id) {
+        Optional<ProjektEntity> optionalQualification = this.repository.findById(id);
+        if (optionalQualification.isEmpty()) {
+            return null;
+        }
+        return optionalQualification.get();
+    }
+
+    public void delete(ProjektEntity entity) {
+        this.repository.delete(entity);
+    }
 }
